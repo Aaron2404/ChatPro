cae6e19c2f4124f2cfc3c84d5b6f4220316ea2ef
Bram <70259613+Bram1903@users.noreply.github.com>
2022-09-04 11:44:04 +0200

    Changed: Made the update checker async

--- src/main/java/dev/boostio/Utils/UpdateChecker.java
+++ src/main/java/dev/boostio/Utils/UpdateChecker.java
@@ -3,42 +3,48 @@ package dev.boostio.Utils;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import dev.boostio.ChatPro;
-import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-// Work in Progress
 public class UpdateChecker {
-
-    static String prefix = ChatPro.PREFIX;
-
-    @SneakyThrows
+    private static final String prefix = ChatColor.GRAY + "[" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Crasher" + ChatColor.GRAY + "] ";
     public static void checkForUpdate() {
-        String version = ChatPro.getInstance().getDescription().getVersion();
-        String parseVersion = version.replace(".", "");
+        Bukkit.getScheduler().runTaskAsynchronously(ChatPro.getInstance(), () -> {
+            String version = ChatPro.getInstance().getDescription().getVersion();
+            String parseVersion = version.replace(".", "");
 
-        String tagName;
-        URL api = new URL("https://api.github.com/repos/BoostioAaron/ChatPro/releases/latest");
-        URLConnection con = api.openConnection();
-        con.setConnectTimeout(15000);
-        con.setReadTimeout(15000);
+            String tagName;
+            URL api;
+            URLConnection con;
+            JsonObject json;
 
-        JsonObject json = new JsonParser().parse(new InputStreamReader(con.getInputStream())).getAsJsonObject();
-        tagName = json.get("tag_name").getAsString();
+            try {
+                api = new URL("https://api.github.com/repos/BoostioAaron/ChatPro/releases/latest");
+                con = api.openConnection();
+                con.setConnectTimeout(15000);
+                con.setReadTimeout(15000);
+                json = new JsonParser().parse(new InputStreamReader(con.getInputStream())).getAsJsonObject();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-        String parsedTagName = tagName.replace(".", "");
+            tagName = json.get("tag_name").getAsString();
+            String parsedTagName = tagName.replace(".", "");
 
-        int latestVersion = Integer.parseInt(parsedTagName.substring(1));
+            int latestVersion = Integer.parseInt(parsedTagName.substring(1));
 
-        if (latestVersion > Integer.parseInt(parseVersion)) {
-            Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "Found a new version " + ChatColor.RED + tagName);
-            Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "https://github.com/BoostioAaron/ChatPro/releases/latest");
-        } else if (latestVersion < Integer.parseInt(parseVersion)) {
-            Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "You are running an unreleased version. You must be someone special! ;-)");
-        }
+            if (latestVersion > Integer.parseInt(parseVersion)) {
+                Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "Found a new version " + ChatColor.RED + tagName);
+                Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "https://github.com/BoostioAaron/ChatPro/releases/latest");
+            } else if (latestVersion < Integer.parseInt(parseVersion)) {
+                Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "You are running an unreleased version. You must be someone special! ;-)");
+            }
+        });
     }
 }
