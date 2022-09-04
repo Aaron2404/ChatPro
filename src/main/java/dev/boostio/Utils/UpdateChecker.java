d8be9a62b92c747ad5944570b07bf59316ba1645
Bram <70259613+Bram1903@users.noreply.github.com>
2022-09-04 11:45:06 +0200

    Update UpdateChecker.java

--- src/main/java/dev/boostio/Utils/UpdateChecker.java
+++ src/main/java/dev/boostio/Utils/UpdateChecker.java
@@ -8,7 +8,6 @@ import org.bukkit.ChatColor;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
