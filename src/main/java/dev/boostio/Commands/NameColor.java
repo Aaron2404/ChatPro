d5104b980dd0644510b27093a34888a662b7e423
Boostio <70571116+BoostioAaron@users.noreply.github.com>
2023-08-09 16:26:30 +0200

    Added: NameColor Enum instead of using strings. Improved: The way item stacks are created.

--- src/main/java/dev/boostio/Commands/NameColor.java
+++ src/main/java/dev/boostio/Commands/NameColor.java
@@ -2,6 +2,7 @@ package dev.boostio.Commands;
 
 import dev.boostio.ChatPro;
 import dev.boostio.Utils.ColoringUtils;
+import dev.boostio.Utils.NameColorEnum;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -38,38 +39,10 @@ public class NameColor implements CommandExecutor {
         player.openInventory(colorSelectionMenu);
         player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 1, 1);
 
-        ItemStack defaultColor = new ItemStack(Material.WOOL, 1, (short) 0);
-        ItemMeta defaultColorMD = defaultColor.getItemMeta();
-        defaultColorMD.setDisplayName(ChatColor.WHITE + "Default");
-        ArrayList<String> defaultLore = new ArrayList<>();
-        defaultLore.add(ChatColor.BLUE + "Click to select color");
-        defaultColorMD.setLore(defaultLore);
-        defaultColor.setItemMeta(defaultColorMD);
-
-        ItemStack lightBlueColor = new ItemStack(Material.WOOL, 1, (short) 3);
-        ItemMeta lightBlueColorMD = lightBlueColor.getItemMeta();
-        lightBlueColorMD.setDisplayName(ChatColor.AQUA + "Aqua");
-        ArrayList<String> aquaLore = new ArrayList<>();
-        aquaLore.add(ChatColor.BLUE + "Click to select color");
-        lightBlueColorMD.setLore(aquaLore);
-        lightBlueColor.setItemMeta(lightBlueColorMD);
-
-        ItemStack greenColor = new ItemStack(Material.WOOL, 1, (short) 5);
-        ItemMeta greenColorMD = greenColor.getItemMeta();
-        greenColorMD.setDisplayName(ChatColor.GREEN + "Green");
-        ArrayList<String> greenLore = new ArrayList<>();
-        greenLore.add(ChatColor.BLUE + "Click to select color");
-        greenColorMD.setLore(greenLore);
-        greenColor.setItemMeta(greenColorMD);
-
-        ItemStack grayColor = new ItemStack(Material.WOOL, 1, (short) 8);
-        ItemMeta grayColorMD = grayColor.getItemMeta();
-        grayColorMD.setDisplayName(ChatColor.GRAY + "Gray");
-        ArrayList<String> grayLore = new ArrayList<>();
-        greenLore.add(ChatColor.BLUE + "Click to select color");
-        grayColorMD.setLore(grayLore);
-        grayColor.setItemMeta(grayColorMD);
-
+        ItemStack defaultColor = CreateColorItem(NameColorEnum.Default, ChatColor.WHITE, 0);
+        ItemStack lightBlueColor = CreateColorItem(NameColorEnum.Aqua, ChatColor.AQUA, 3);
+        ItemStack greenColor = CreateColorItem(NameColorEnum.Green, ChatColor.GREEN, 5);
+        ItemStack grayColor = CreateColorItem(NameColorEnum.Gray, ChatColor.GRAY, 8);
 
         // Add Items to the class menu
         colorSelectionMenu.setItem(0, defaultColor);
@@ -78,5 +51,16 @@ public class NameColor implements CommandExecutor {
         colorSelectionMenu.setItem(3, greenColor);
 
         return false;
+    }
+
+    private ItemStack CreateColorItem(NameColorEnum nameColor, ChatColor chatColor, int woolVariant){
+        ItemStack ColorItem = new ItemStack(Material.WOOL, 1, (short) woolVariant);
+        ItemMeta ColorItemMD = ColorItem.getItemMeta();
+        ColorItemMD.setDisplayName(chatColor + nameColor.toString());
+        ArrayList<String> defaultLore = new ArrayList<>();
+        defaultLore.add(ChatColor.BLUE + "Click to select color");
+        ColorItemMD.setLore(defaultLore);
+        ColorItem.setItemMeta(ColorItemMD);
+        return ColorItem;
     }
 }
---
+++ src/main/java/dev/boostio/Utils/NameColorEnum.java
@@ -0,0 +1,8 @@
+package dev.boostio.Utils;
+
+public enum NameColorEnum {
+    Default,
+    Gray,
+    Aqua,
+    Green
+}
