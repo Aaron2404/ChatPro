80fe9bef8d74c5ef8c945d365712b159e0837d5e
Boostio <aaron.laan@gmail.com>
2024-04-12 15:51:06 +0200

    Add: documentation. Removed: redundant functions.

--- src/main/java/dev/boostio/events/AsyncPlayerChat.java
+++ src/main/java/dev/boostio/events/AsyncPlayerChat.java
@@ -7,7 +7,6 @@ import dev.boostio.utils.ColoringUtils;
 import dev.boostio.utils.FormatTypes;
 import dev.boostio.utils.IPv4ValidatorRegex;
 import dev.boostio.utils.PlayerData;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,10 +18,18 @@ import java.util.List;
 public class AsyncPlayerChat implements Listener {
     private final ConfigManager configManager;
 
+    /**
+     * Constructor for AsyncPlayerChat.
+     * @param plugin The ChatPro plugin instance.
+     */
     public AsyncPlayerChat(ChatPro plugin){
         this.configManager = plugin.getConfigManager();
     }
 
+    /**
+     * Event handler for AsyncPlayerChatEvent.
+     * @param event The AsyncPlayerChatEvent instance.
+     */
     @EventHandler
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
@@ -36,10 +43,13 @@ public class AsyncPlayerChat implements Listener {
         formatMessage(event, player);
     }
 
-
-
+    /**
+     * Replaces colors and formats in the player's message.
+     * @param event The AsyncPlayerChatEvent instance.
+     * @param player The Player instance.
+     */
     private void replaceColorsAndFormats(AsyncPlayerChatEvent event, Player player) {
-        if (getColoursEnabled()) {
+        if (configManager.getConfigurationOption(ConfigOption.COLOURS_ENABLED)) {
             if (player.hasPermission("chatpro.colors")) {
                 event.setMessage(ColoringUtils.setColor(event.getMessage()));
             }
@@ -49,24 +59,34 @@ public class AsyncPlayerChat implements Listener {
         }
     }
 
+    /**
+     * Formats the player's message.
+     * @param event The AsyncPlayerChatEvent instance.
+     * @param player The Player instance.
+     */
     private void formatMessage(AsyncPlayerChatEvent event, Player player) {
         PlayerData data = ChatPro.getInstance().getPlayerData().get(player.getUniqueId());
         ChatColor chatColor = data.getChatColorName();
 
         String format = "<" + chatColor + player.getDisplayName() + ChatColor.WHITE + "> " + event.getMessage();
 
-        if (getBetterMessageFormat()) {
+        if (configManager.getConfigurationOption(ConfigOption.BETTER_MESSAGE_FORMAT)) {
             format = chatColor + player.getDisplayName() + ChatColor.WHITE + ": " + event.getMessage();
         }
 
         event.setFormat(format);
     }
 
+    /**
+     * Filters IPs in the player's message.
+     * @param event The AsyncPlayerChatEvent instance.
+     * @param player The Player instance.
+     */
     private void filterIPs(AsyncPlayerChatEvent event, Player player) {
         String playerMessage = event.getMessage().toLowerCase();
         String[] splitWords = playerMessage.split(" ");
 
-        if (getFilterIp()) {
+        if (configManager.getConfigurationOption(ConfigOption.FILTER_IP)) {
             for (String separateWord : splitWords) {
                 boolean ipFound = IPv4ValidatorRegex.isValid(separateWord);
                 if (ipFound) {
@@ -85,6 +105,11 @@ public class AsyncPlayerChat implements Listener {
         }
     }
 
+    /**
+     * Filters words in the player's message.
+     * @param event The AsyncPlayerChatEvent instance.
+     * @param player The Player instance.
+     */
     private void filterWords(AsyncPlayerChatEvent event, Player player) {
         String[] filteredWords = getFilteredWords();
         String playerMessage = event.getMessage().toLowerCase();
@@ -93,7 +118,7 @@ public class AsyncPlayerChat implements Listener {
             String filterWordString = word.toLowerCase();
             if (playerMessage.contains(filterWordString)) {
                 if (getBlockMessage()) {
-                    player.sendMessage(getBlockedMessageNotification());
+                    player.sendMessage(ChatColor.RED + getBlockedMessageNotification());
                     event.setCancelled(true);
                 }
 
@@ -105,36 +130,44 @@ public class AsyncPlayerChat implements Listener {
         }
     }
 
+    /**
+     * Returns the list of filtered words.
+     * @return An array of filtered words.
+     */
     private String[] getFilteredWords() {
         List<String> filteredWordsList = configManager.getConfigurationOption(ConfigOption.FILTERED_WORDS);
         return filteredWordsList.toArray(new String[0]);
     }
 
-    private boolean getFilterIp() {
-        return configManager.getConfigurationOption(ConfigOption.FILTER_IP);
-    }
-
+    /**
+     * Returns the block message configuration option.
+     * @return A boolean indicating whether the block message option is enabled.
+     */
     private boolean getBlockMessage() {
         return configManager.getConfigurationOption(ConfigOption.BLOCK_MESSAGE);
     }
 
+    /**
+     * Returns the replacement word in message configuration option.
+     * @return A boolean indicating whether the replacement word in message option is enabled.
+     */
     private boolean getReplaceWordInMessage() {
         return configManager.getConfigurationOption(ConfigOption.REPLACE_WORD_IN_MESSAGE);
     }
 
+    /**
+     * Returns the filtered word replacement configuration option.
+     * @return A string representing the filtered word replacement.
+     */
     private String getFilteredWordReplacement() {
         return configManager.getConfigurationOption(ConfigOption.FILTERED_WORD_REPLACEMENT);
     }
 
+    /**
+     * Returns the blocked message notification configuration option.
+     * @return A string representing the blocked message notification.
+     */
     private String getBlockedMessageNotification() {
         return configManager.getConfigurationOption(ConfigOption.BLOCKED_MESSAGE_NOTIFICATION);
-    }
-
-    private boolean getBetterMessageFormat() {
-        return configManager.getConfigurationOption(ConfigOption.BETTER_MESSAGE_FORMAT);
-    }
-
-    private boolean getColoursEnabled() {
-        return configManager.getConfigurationOption(ConfigOption.COLOURS_ENABLED);
     }
 }
