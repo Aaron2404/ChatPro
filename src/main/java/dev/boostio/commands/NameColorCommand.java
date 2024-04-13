20241bb5a15feea69a07ecfb34bf6691062f1f2b
Boostio <aaron.laan@gmail.com>
2024-04-13 22:08:53 +0200

    Optimize imports

--- src/main/java/dev/boostio/commands/NameColorCommand.java
+++ src/main/java/dev/boostio/commands/NameColorCommand.java
@@ -4,28 +4,14 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
-import com.github.retrooper.packetevents.PacketEvents;
-import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import dev.boostio.ChatPro;
-import dev.boostio.managers.ChatManager;
 import dev.boostio.managers.ColorManager;
-import dev.boostio.utils.ColoringUtils;
-import dev.boostio.enums.NameColorEnum;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @CommandAlias("namecolor|nc")
 @CommandPermission("chatpro.namecolor")
